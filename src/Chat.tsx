d9f2dde515cd2c2714e63c104b4148a5ab0bd6d9
deguchi <deguchik@gmail.com>
2021-08-03 15:53:01 +0900

    update

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -1,91 +1,37 @@
 import React, { useState, useEffect } from 'react'
 
-// RCE CSS
-import 'react-chat-elements/dist/main.css';
-// MessageBox component
-import { SideBar, MessageList, MessageBox, ChatItem, SystemMessage, Avatar } from 'react-chat-elements';
 
+import 'react-chat-elements/dist/main.css';
 
 const Chat = (props) => {
     return (
         <div id="chat">
             <h1>Chat</h1>
-            <MessageBox />
-            <ChatItem
-                avatar={'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K'}
-                alt={'Reactjs'}
-                title={'Facebook'}
-                subtitle={'What are you doing?'}
-                date={new Date()}
-                unread={0} />
-
-            <MessageBox
-                position={'left'}
-                type={'photo'}
-                text={'react.svg'}
-                data={{
-                    uri: 'https://facebook.github.io/react/img/logo.svg',
-                    status: {
-                        click: false,
-                        loading: 0,
-                    }
-                }} />
-
-            <MessageBox
-                reply={{
-                    photoURL: 'https://facebook.github.io/react/img/logo.svg',
-                    title: 'elit magna',
-                    titleColor: '#8717ae',
-                    message: 'Aliqua amet incididunt id nostrud',
-                }}
-                onReplyMessageClick={() => console.log('reply clicked!')}
-                position={'left'}
-                type={'text'}
-                text={'Tempor duis do voluptate enim duis velit veniam aute ullamco dolore duis irure.'} />
-            <SystemMessage
-                text={'End of conversation'} />
-            <MessageList
-                className='message-list'
-                lockable={true}
-                toBottomHeight={'100%'}
-                dataSource={[
-                    {
-                        position: 'right',
-                        type: 'text',
-                        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
-                        date: new Date(),
-                    },
-                    {
-                        position: 'left',
-                        type: 'text',
-                        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
-                        date: new Date(),
-                    },
-                    {
-                        position: 'right',
-                        type: 'text',
-                        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
-                        date: new Date(),
-                    },
-                ]} />
-            <SideBar
-                top={
-                    <div>'TOP' area</div>
-                }
-                center={
-                    <div>'CENTER' area</div>
-                }
-                bottom={
-                    <div>'BOTTOM' area</div>
-                } />
-                <Avatar
-    src={'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K'}
-    alt={'logo'}
-    size="large"
-    type="circle flexible"/>
+            <div className="screen">
+                <div className="conversation">
+                    <div className="messages messages--received">
+                        <div className="message">This codepen is an exemple of</div>
+                        <div className="message">how to create the Facebook thumb up</div>
+                    </div>
+                    <div className="messages messages--sent">
+                        <div className="message">Try to type</div>
+                        <div className="message">or click the thumb up!</div>
+                        <div className="message">;)</div>
+                    </div>
+                    <div className="messages messages--received">
+                        <div className="message">Enjoy!</div>
+                    </div>
+                </div>
+                <div className="text-bar">
+                    <form className="text-bar__field" id="form-message">
+                        <input type="text" placeholder="Type or thumb up ;)" />
+                    </form>
+                    <div className="text-bar__thumb">
+                        <div className="thumb"></div>
+                    </div>
+                </div>
+            </div>
         </div>
-
-
     )
 }
 
