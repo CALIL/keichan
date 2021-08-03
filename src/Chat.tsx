ebd4174a20dfdf91f2ee56f143f2261976d47443
deguchi <deguchik@gmail.com>
2021-08-03 11:37:17 +0900

    Chat

---
+++ src/Chat.tsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+const Chat = (props) => {
+    return (
+        <h1>Chat</h1>
+    )
+}
+
+export default Chat
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import {flowPlugin, esbuildFlowPlugin} from '@bunchtogether/vite-plugin-flow'
 import legacy from '@vitejs/plugin-legacy'
+const { resolve } = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,5 +15,21 @@ export default defineConfig({
         targets: ['ie >= 11'],
         additionalLegacyPolyfills: ['regenerator-runtime/runtime']
     })],
-    base: '/keichan/'
+    base: '/keichan/',
+    build: {
+        outDir: 'build',
+        rollupOptions: {
+          input: {
+            // need a better way to template
+            main: resolve(__dirname, 'index.html'),
+            page: resolve(__dirname, 'chat.html'),
+          },
+        //   // ファイル名にハッシュを付けない
+        //   output: {
+        //     entryFileNames: `assets/[name].js`,
+        //     chunkFileNames: `assets/[name].js`,
+        //     assetFileNames: `assets/[name].[ext]`
+        //   }
+        },
+    },
 })
