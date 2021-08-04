aeb5329447943a0c90ecfcd0727034cbc583a1db
deguchi <deguchik@gmail.com>
2021-08-04 14:27:05 +0900

    update

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -1,9 +1,22 @@
 import React, { useState, useEffect } from 'react'
 
+
+import { Button, Intent, Spinner } from "@blueprintjs/core";
+ 
+
+// using JSX:
+// const mySpinner = <Spinner intent={Intent.PRIMARY} />;
+ 
+// use React.createElement if you're not using JSX.
+// const myButton = React.createElement(Button, { intent: Intent.SUCCESS }, "button content");
+
 const Chat = (props) => {
     return (
         <div id="chat">
-            <div className="excel"></div>
+            <div className="excel">
+                <Button>Click me!</Button>
+
+            </div>
             <div className="screen">
                 <div className="conversation">
                     <div className="messages messages--received">
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ const { resolve } = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    define: {
+        'process.env': {}
+    },
     optimizeDeps: {
         esbuildOptions: {
             plugins: [esbuildFlowPlugin()]
