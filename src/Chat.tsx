2a417b040c75452270ebf84550bee77dd8e10a8b
deguchi <deguchik@gmail.com>
2021-08-04 10:30:20 +0900

    update

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -3,19 +3,29 @@ import React, { useState, useEffect } from 'react'
 const Chat = (props) => {
     return (
         <div id="chat">
+            <div className="excel"></div>
             <div className="screen">
                 <div className="conversation">
                     <div className="messages messages--received">
-                        <div className="message">This codepen is an exemple of</div>
-                        <div className="message">how to create the Facebook thumb up</div>
+                        <div className="message">おはようございます</div>
+                        <div className="message">バーコードリーダーが接続されました</div>
                     </div>
                     <div className="messages messages--sent">
-                        <div className="message">Try to type</div>
-                        <div className="message">or click the thumb up!</div>
-                        <div className="message">;)</div>
+                        <div className="message">読み込み</div>
+                        <div className="message">9784103534228</div>
                     </div>
                     <div className="messages messages--received">
-                        <div className="message">Enjoy!</div>
+                        <div className="message">ISBNを読み込みました</div>
+                        <div className="message">本が見つかりました!</div>
+                        <div className="message book">
+                            <img className="thumbnail" src="https://cover.openbd.jp/9784103534228.jpg" alt="" />
+                            <div>
+                                <span className="title">1Q84 BOOK 1</span>
+                                <span className="author">村上 春樹  (著)</span>
+                                <span className="publisher">新潮社</span>
+                                <span className="pubdate">2009/5/29</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
