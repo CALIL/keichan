30c830b836af432147524cef1c0c01c3b4b541d6
deguchi <deguchik@gmail.com>
2021-08-25 10:26:09 +0900

    update

---
+++ src/suggest/App.tsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react'
+
+import { Button, Intent, Spinner, Card, Elevation, Tag, Icon, InputGroup } from "@blueprintjs/core";
+
+const App = (props) => {
+    return (
+        <div id="chat">
+            <header>
+                <h1>カーリルtoolbox: keichan</h1>
+            </header>
+            <main>
+            </main>
+        </div>
+    )
+}
+
+export default App
