e8a899d2c9547729d96cbd1222f9a6ae4d42b426
deguchi <deguchik@gmail.com>
2021-08-25 15:42:36 +0900

    update

--- src/suggest/App.tsx
+++ src/suggest/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 
-import { Button, Intent, Spinner, Card, Elevation, Tag, Icon, InputGroup } from "@blueprintjs/core";
+import { Button, Intent, Spinner, Card, Elevation, Tag, Icon, InputGroup, FormGroup } from "@blueprintjs/core";
 
 import data from './search.json'
 
@@ -29,9 +29,6 @@ const App = (props) => {
                     </div>
                 </div>
                 <div className="main">
-                    {/* <form action="">
-                        <input type="text" className="bp3-input" value="9784088820118" />
-                    </form> */}
                     <Card className="card active" interactive={true} elevation={Elevation.TWO}>
                         <div>
                             <Tag className="tag" large>100014</Tag>
@@ -48,9 +45,27 @@ const App = (props) => {
                         </div>
                         <Icon icon="delete" size={25} color={'#ffffff'} />
                     </Card> */}
+                    <form action="">
+                        <div className="bp3-input-group modifier">
+                            <span className="bp3-icon bp3-icon-search"></span>
+                            <input className="bp3-input" type="search" placeholder="キーワード or ISBN..." dir="auto" />
+                        </div>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path d="M176 352c53.02 0 96-42.98 96-96V96c0-53.02-42.98-96-96-96S80 42.98 80 96v160c0 53.02 42.98 96 96 96zm160-160h-16c-8.84 0-16 7.16-16 16v48c0 74.8-64.49 134.82-140.79 127.38C96.71 376.89 48 317.11 48 250.3V208c0-8.84-7.16-16-16-16H16c-8.84 0-16 7.16-16 16v40.16c0 89.64 63.97 169.55 152 181.69V464H96c-8.84 0-16 7.16-16 16v16c0 8.84 7.16 16 16 16h160c8.84 0 16-7.16 16-16v-16c0-8.84-7.16-16-16-16h-56v-33.77C285.71 418.47 352 344.9 352 256v-48c0-8.84-7.16-16-16-16z"/></svg>
+                    </form>
                     <div className="addMore">
-                        <InputGroup small placeholder="タイトル著者名を自分で入力" />
-                        <InputGroup small placeholder="メモを追加" />
+                        
+                        <FormGroup
+                            helperText=""
+                            label="タイトル著者名を自分で入力"
+                            labelFor="text-input"
+                            labelInfo=""
+                        >
+                            <InputGroup small placeholder="タイトル" />
+                            <InputGroup small placeholder="著者名" />
+                            <InputGroup small placeholder="出版社" />
+                            <InputGroup small placeholder="出版日" />
+                            <InputGroup small placeholder="メモを追加" />
+                        </FormGroup>
                     </div>
                     <div className="nextBook">
                         <h2>もしかして<span>(SPY×FAMILY 1より推定)</span></h2>
