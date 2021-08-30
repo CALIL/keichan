8298f53964d6499d3712cebc570f7a390f90c723
deguchi <deguchik@gmail.com>
2021-08-30 14:48:55 +0900

    update

--- src/suggest/App.tsx
+++ src/suggest/App.tsx
@@ -247,8 +247,8 @@ const App = (props) => {
                                             <div>
                                                 <h3>{[book.title, book.volume].join(' ')}</h3>
                                                 <p className="author">{book.author}</p>
-                                                {/* <p>{book.pubdate}</p> */}
-                                                <p>{book.isbn}</p>
+                                                <p>{book.pubdate}</p>
+                                                {/* <p>{book.isbn}</p> */}
                                             </div>
                                         </div>
                                         <Icon icon="add" size={25} color={'#ffffff'} />
